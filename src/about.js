bb98d3c6ba9ddd2b2df4d3ccfe20993a38a045e7
“Rebecca <rebeccaschiffman@gmail.com>
2022-04-30 14:06:38 -0700

    Start adding to about page and styling

--- src/about.js
+++ src/about.js
@@ -5,19 +5,24 @@ function about() {
   aboutbox.classList.add('sectionbox');
   aboutbox.id = 'aboutbox';
 
-  const paragraph = document.createElement('p');
+  const paragraph1 = document.createElement('p');
+  const paragraph2 = document.createElement('p');
 
   // Fill out componenet here.
-  paragraph.innerHTML = ('The Pickle is a casual lunch spot, ideal for enjoying food, conversation, and work.');
-  paragraph.classList.add('about');
+  paragraph1.textContent = ('The Pickle is a casual breakfast and lunch cafe, ideal for enjoying food, conversation, and work. The space is divided into three areas, one that accomodates larger groups, another for four diners and less, and then small cafe tables with outlets.');
+  paragraph1.classList.add('aboutp');
+  paragraph2.textContent = ('All of our fruits and vegetables are sourced from local farmers. We compost our scraps. The Pickle is a vegan establishment. We do not use any ingredients from animals.');
+  paragraph2.classList.add('aboutp');
 
   // Add the image to our existing div.
   const myIcon = new Image();
   myIcon.src = Icon;
   myIcon.classList.add('imageblock');
 
-  aboutbox.appendChild(paragraph);
+  aboutbox.appendChild(paragraph1);
   aboutbox.appendChild(myIcon);
+  aboutbox.appendChild(paragraph2);
+ 
 
   return aboutbox;
 }
